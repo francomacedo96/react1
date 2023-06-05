c424eeabd78791788db2f540988638157effa4ab
francomacedo96 <francomacedo@outlook.com>
2023-06-05 15:47:06 -0300

    fin

--- app-uno/src/App.js
+++ app-uno/src/App.js
@@ -7,8 +7,29 @@ function App() {
     <div className="App">
         <div className="contenedor-principal">
 
-          <h1>Obras de Basquiat</h1>
-          <Testimonio />
+          <Testimonio 
+          nombre="Calavera"
+          imagen="uno"
+          pais="Estados Unidos"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus ornare commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis nunc sit amet velit tempor ornare vitae non arcu. Fusce nunc nunc, tempor et rhoncus et, ullamcorper quis erat. Phasellus at elementum velit, sed rhoncus lectus. Nullam fringilla ligula lacus, sed mollis est congue vitae. Cras porta, velit sit amet molestie fermentum, arcu nisl vulputate metus, nec ultricies leo dolor ut tortor.
+          "
+          />
+
+          <Testimonio 
+          nombre="Calavera"
+          imagen="dos"
+          pais="Estados Unidos"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus ornare commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis nunc sit amet velit tempor ornare vitae non arcu. Fusce nunc nunc, tempor et rhoncus et, ullamcorper quis erat. Phasellus at elementum velit, sed rhoncus lectus. Nullam fringilla ligula lacus, sed mollis est congue vitae. Cras porta, velit sit amet molestie fermentum, arcu nisl vulputate metus, nec ultricies leo dolor ut tortor.
+          "
+          />
+
+          <Testimonio 
+          nombre="Calavera"
+          imagen="tres"
+          pais="Japon"
+          testimonio="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas finibus ornare commodo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis nunc sit amet velit tempor ornare vitae non arcu. Fusce nunc nunc, tempor et rhoncus et, ullamcorper quis erat. Phasellus at elementum velit, sed rhoncus lectus. Nullam fringilla ligula lacus, sed mollis est congue vitae. Cras porta, velit sit amet molestie fermentum, arcu nisl vulputate metus, nec ultricies leo dolor ut tortor.
+          "
+          />
         </div>
 
     </div>
--- app-uno/src/componentes/Testimonio.jsx
+++ app-uno/src/componentes/Testimonio.jsx
@@ -1,15 +1,13 @@
 import React from "react";
+import "../estilo/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
     return (
       <div className="contenedor-testimonio">
-        <img className="imagen-testimonio" src={require("../imagenes/basquiat-uno.jpg")} alt="Foto basquiat uno" />
+        <img className="imagen-testimonio" src={require(`../imagenes/basquiat-${props.imagen}.jpg`)} alt="Foto basquiat uno" />
         <div className="contenedor-texto-testimonio">
-          <p className="nombre-testimonio">Franco</p>
-          <p className="cargo-testimonio">Nada</p>
-          <p className="texto-testimonio">Quisque consequat varius dui, id sollicitudin libero lobortis nec. Vivamus et eros at odio pulvinar eleifend ut nec dui. Nunc blandit auctor mi sed molestie. Donec congue mauris sed augue vulputate, ut dignissim elit lacinia. Donec fringilla ligula lorem. Curabitur tellus lorem, pharetra vitae malesuada vitae, consectetur sed purus. Proin vitae magna nunc. In eu velit a elit hendrerit molestie sit amet sed urna. Donec eleifend nisl ut pulvinar placerat. Quisque sollicitudin molestie felis, sit amet varius mauris venenatis ac. Maecenas id enim quis arcu porta lacinia.
-
-</p>
+          <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+          <p className="texto-testimonio">{props.testimonio}</p>
         </div>
       </div>
     );
